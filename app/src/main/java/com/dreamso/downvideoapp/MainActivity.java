12ba4625307059ad1a966269ead26d182a4898ca
chashikajw <chashikajw007@gmail.com>
2019-05-12 00:33:44 +0530

    intialise the project

---
+++ app/src/main/java/com/dreamso/downvideoapp/MainActivity.java
@@ -0,0 +1,65 @@
+package com.dreamso.downvideoapp;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.dreamso.downvideoapp.fragments.HomeFragment;
+import com.dreamso.downvideoapp.fragments.MyFileFragment;
+import com.dreamso.downvideoapp.fragments.SubscriptionFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new HomeFragment()); //getting bottom navigation view and attaching the listener
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new SubscriptionFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new MyFileFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+}
---
+++ app/src/main/java/com/dreamso/downvideoapp/fragments/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.dreamso.downvideoapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dreamso.downvideoapp.R;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+}
