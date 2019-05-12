6395957f6f4ddc3fc98f34a93e1be451e89184b1
chashikajw <chashikajw007@gmail.com>
2019-05-12 16:37:35 +0530

    implement http request

--- app/src/main/java/com/dreamso/downvideoapp/fragments/tabFragments/TrendingTabFragment.java
+++ app/src/main/java/com/dreamso/downvideoapp/fragments/tabFragments/TrendingTabFragment.java
@@ -1,18 +1,44 @@
 package com.dreamso.downvideoapp.fragments.tabFragments;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dreamso.downvideoapp.R;
+import com.dreamso.downvideoapp.fragments.adapters.VideoPostAdapter;
+import com.dreamso.downvideoapp.fragments.models.YoutubeDataModel;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TrendingTabFragment extends Fragment {
+
+    private static String GOOGLE_YOUTUBE_API_KEY = "AIzaSyDY5KEE5eQbAxowrjoQY7L-N3wql81I45k";//here you should use your api key for testing purpose you can use this api also
+    private static String CHANNEL_ID = "UCoMdktPbSTixAyNGwb-UYkQ"; //here you should use your channel id for testing purpose you can use this api also
+    private static String CHANNLE_GET_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&channelId=" + CHANNEL_ID + "&maxResults=20&key=" + GOOGLE_YOUTUBE_API_KEY + "";
+
+    private RecyclerView mList_videos = null;
+    private VideoPostAdapter adapter = null;
+    private ArrayList<YoutubeDataModel> mListData = null;
 
 
     public TrendingTabFragment() {
@@ -23,8 +49,64 @@ public class TrendingTabFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragmenttab_trending, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragmenttab_trending, container, false);
+
+        mList_videos = (RecyclerView) rootView.findViewById(R.id.mList_videos);
+        initList();
+        new RequestYoutubeAPI().execute();
+        return rootView;
+    }
+
+    private void initList() {
+        mListData = new ArrayList<>();
+        mList_videos.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new VideoPostAdapter(getActivity(),mListData);
+        mList_videos.setAdapter(adapter);
+
+
+    }
+
+
+    //create an asynctask to get all the data from youtube
+    private class RequestYoutubeAPI extends AsyncTask<Void, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(CHANNLE_GET_URL);
+            Log.e("URL", CHANNLE_GET_URL);
+            try {
+                HttpResponse response = httpClient.execute(httpGet);
+                HttpEntity httpEntity = response.getEntity();
+                String json = EntityUtils.toString(httpEntity);
+                return json;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String response){
+            super.onPostExecute(response);
+            if(response != null){
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                    Log.e("response",jsonObject.toString());
+
+
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
